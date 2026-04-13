36ed1df6f2288ba250f844ef8c3233fe79dce855
SASUKE38 <99054872+SASUKE38@users.noreply.github.com>
2026-04-12 23:45:57 -0700

    documentation collection setup

--- server/src/documentation/documentationProvider.ts
+++ server/src/documentation/documentationProvider.ts
@@ -1,7 +1,8 @@
+import { Cheerio, CheerioAPI, load } from "cheerio";
 import { ComponentBase } from "../componentBase";
-import { Server } from '../server';
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import { Server } from "../server";
+import axios from "axios";
+import { Element } from "domhandler";
 
 export class DocumentationProvider extends ComponentBase {
 	readonly urlBase = new URL("https://docs.baldursgate3.game");
@@ -11,53 +12,76 @@ export class DocumentationProvider extends ComponentBase {
 	constructor(server: Server) {
 		super(server);
 
-		axios.interceptors.response.use((response) => {
-			return response;
-		}, (error) => {
-			console.error(error.message);
-			return Promise.reject(error);
-		});
+		axios.interceptors.response.use(
+			(response) => {
+				return response;
+			},
+			(error) => {
+				console.error(error.message);
+				return Promise.reject(error);
+			}
+		);
 	}
 
 	async getCategoryLinks(title: string) {
 		const url = this.getSearchURL(title);
 		const searchURLs: string[] = [];
 		axios.get(url.toString()).then(async (response) => {
-			const $ = cheerio.load(response.data);
-			$('.mw-category-group').children().children().children().each((i, element) => {
-				const link = $(element).attr("href");
-				if (link) searchURLs.push(link as string);
-			})
-			await Promise.all(searchURLs.map(async (url) => { this.getFunctionDocumentation(url) }))
-		})
+			const $ = load(response.data);
+			$(".mw-category-group")
+				.children()
+				.children()
+				.children()
+				.each((i, element) => {
+					const link = $(element).attr("href");
+					if (link) searchURLs.push(link as string);
+				});
+			await Promise.all(
+				searchURLs.map(async (url) => {
+					this.getFunctionDocumentation(url);
+				})
+			);
+		});
 	}
 
 	async getFunctionDocumentation(title: string): Promise<string> {
 		const url = this.getSearchURL(title);
-		axios.get(url.toString()).then((response) => {
-			const $ = cheerio.load(response.data);
-			const $content = $('.mw-body-content')
-				.first()
-				.children()
-				.children()
-				.filter((_, element) => {
-					return !$(element).hasClass('toc');
-				});
-			return $content.text();
-		}).catch((error) => {
-			console.error("Failed to fetch documentation.")
-		});
+		axios
+			.get(url.toString())
+			.then((response) => {
+				const $ = load(response.data);
+				const $content = $(".mw-body-content")
+					.first()
+					.children()
+					.children()
+					.filter((_, element) => {
+						return !$(element).hasClass("toc");
+					});
+				this.parseFunctionDocumentation($content, $);
+				return $content.text();
+			})
+			.catch(() => {
+				console.error("Failed to fetch documentation.");
+			});
 		return "";
 	}
 
 	private getSearchURL(query: string) {
 		const url = new URL(this.urlBase);
+		url.search = this.urlSearchPrefix;
 		if (query.startsWith(this.urlPathName + this.urlSearchPrefix)) {
-			url.search = this.urlSearchPrefix + query.substring(this.urlPathName.length + this.urlSearchPrefix.length);
+			url.search += query.substring(this.urlPathName.length + this.urlSearchPrefix.length);
 		} else {
-			url.search = this.urlSearchPrefix + query;
+			url.search += query;
 		}
 		url.pathname = this.urlPathName;
 		return url;
+	}
+
+	private parseFunctionDocumentation(elements: Cheerio<Element>, $: CheerioAPI) {
+		elements.each((i, el) => {
+			const text = $(el).text();
+			if (text === "Full Definitions") console.log($(el).text());
+		});
 	}
 }
