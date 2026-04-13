52b12779b89782ffc00e99ce85cf2652589b0797
SASUKE38 <bpstern@uci.edu>
2026-04-12 22:48:43 -0700

    initial category scraping

--- server/src/documentation/documentationProvider.ts
+++ server/src/documentation/documentationProvider.ts
@@ -4,35 +4,60 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 
 export class DocumentationProvider extends ComponentBase {
-	readonly urlBase = new URL("https://docs.baldursgate3.game/index.php");
+	readonly urlBase = new URL("https://docs.baldursgate3.game");
+	readonly urlPathName = "/index.php";
 	readonly urlSearchPrefix = "?title=";
 
 	constructor(server: Server) {
 		super(server);
+
+		axios.interceptors.response.use((response) => {
+			return response;
+		}, (error) => {
+			console.error(error.message);
+			return Promise.reject(error);
+		});
+	}
+
+	async getCategoryLinks(title: string) {
+		const url = this.getSearchURL(title);
+		const searchURLs: string[] = [];
+		axios.get(url.toString()).then(async (response) => {
+			const $ = cheerio.load(response.data);
+			$('.mw-category-group').children().children().children().each((i, element) => {
+				const link = $(element).attr("href");
+				if (link) searchURLs.push(link as string);
+			})
+			await Promise.all(searchURLs.map(async (url) => { this.getFunctionDocumentation(url) }))
+		})
 	}
 
 	async getFunctionDocumentation(title: string): Promise<string> {
-		const url = new URL(this.urlBase);
-		url.search = this.urlSearchPrefix + title;
+		const url = this.getSearchURL(title);
 		axios.get(url.toString()).then((response) => {
 			const $ = cheerio.load(response.data);
 			const $content = $('.mw-body-content')
 				.first()
 				.children()
 				.children()
-				.filter((i, element) => {
+				.filter((_, element) => {
 					return !$(element).hasClass('toc');
 				});
 			return $content.text();
 		}).catch((error) => {
-			if (error.response) {
-				console.error(`Status ${error.response.status} received for ${url}`);
-			} else if (error.request) {
-				console.error(`No response for ${url}`);
-			} else {
-				console.error('Error', error.message);
-			}
+			console.error("Failed to fetch documentation.")
 		});
 		return "";
+	}
+
+	private getSearchURL(query: string) {
+		const url = new URL(this.urlBase);
+		if (query.startsWith(this.urlPathName + this.urlSearchPrefix)) {
+			url.search = this.urlSearchPrefix + query.substring(this.urlPathName.length + this.urlSearchPrefix.length);
+		} else {
+			url.search = this.urlSearchPrefix + query;
+		}
+		url.pathname = this.urlPathName;
+		return url;
 	}
 }
