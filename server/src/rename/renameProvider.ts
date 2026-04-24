c2c6aa9c8dfd1fd11582869dc98966144ca7ff6c
SASUKE38 <bpstern@uci.edu>
2026-04-24 10:52:53 -0700

    rename provider cleaning

--- server/src/rename/renameProvider.ts
+++ server/src/rename/renameProvider.ts
@@ -21,47 +21,30 @@ export class RenameProvider extends ComponentBase {
 		return await this.server.symbolManager.validateRenameOrReferences(params);
 	};
 
-	private handleRenameRequest = async (params: RenameParams): Promise<WorkspaceEdit> => {
+	private handleRenameRequest = async (params: RenameParams): Promise<WorkspaceEdit | null> => {
 		const symbolsAt = await this.server.symbolManager.getSymbolsAt(params.textDocument, params.position);
-		const documentURI = params.textDocument.uri;
 		const oldSymbol = symbolsAt[symbolsAt.length - 1];
-		const res: WorkspaceEdit = { changes: { [`${documentURI}`]: [] } };
 
 		if (oldSymbol.kind === SymbolKind.Variable || oldSymbol.kind === SymbolKind.Constant) {
-			return Promise.resolve(this.renameVariableOrConstant(params, symbolsAt, oldSymbol, documentURI));
+			return Promise.resolve(this.renameVariableOrConstant(params, symbolsAt, oldSymbol));
 		} else if (oldSymbol.kind === SymbolKind.Function) {
-			return await this.renameSignature(params, oldSymbol, documentURI);
+			return await this.renameSignature(params, oldSymbol);
 		}
 
-		return res;
+		return null;
 	};
 
-	private async renameSignature(
-		params: RenameParams,
-		oldSymbol: DocumentSymbol,
-		documentURI: string
-	): Promise<WorkspaceEdit> {
-		const allSymbols = await this.server.symbolManager.getAllSymbols(documentURI);
+	private async renameSignature(params: RenameParams, oldSymbol: DocumentSymbol): Promise<WorkspaceEdit> {
+		const allSymbols = await this.server.symbolManager.getAllSymbols(params.textDocument.uri);
 		const res: WorkspaceEdit = { changes: {} };
-
-		function doRenameTraversal(path: string, symbol: DocumentSymbol) {
-			if (!symbol.children) return;
-			for (const child of symbol.children) {
-				if (child.name === oldSymbol.name) {
-					res.changes![`${path}`].push({ range: child.selectionRange, newText: params.newName });
-				} else {
-					if (child.kind != SymbolKind.Function) {
-						doRenameTraversal(path, child);
-					}
-				}
-			}
-		}
 
 		for (const entry of allSymbols.entries()) {
 			const encodedPath = encodePath(entry[0]);
 			res.changes![`${encodedPath}`] = [];
 			for (const section of entry[1]) {
-				doRenameTraversal(encodedPath, section);
+				for (const range of this.server.symbolManager.findSignatureUsages(section, oldSymbol)) {
+					res.changes![`${encodedPath}`].push({ range: range, newText: params.newName });
+				}
 			}
 		}
 
@@ -71,27 +54,12 @@ export class RenameProvider extends ComponentBase {
 	private renameVariableOrConstant(
 		params: RenameParams,
 		symbolsAt: DocumentSymbol[],
-		oldSymbol: DocumentSymbol,
-		documentURI: string
+		oldSymbol: DocumentSymbol
 	): WorkspaceEdit {
-		const res: WorkspaceEdit = { changes: { [`${documentURI}`]: [] } };
+		const res: WorkspaceEdit = { changes: { [`${params.textDocument.uri}`]: [] } };
 
-		for (let i = symbolsAt.length - 1; i >= 0; i--) {
-			if (symbolsAt[i].kind === SymbolKind.Class) {
-				if (!symbolsAt[i].children) return {};
-				for (const func of symbolsAt[i].children!) {
-					if (!func.children) return {};
-					for (const parameter of func.children) {
-						if (parameter.name === oldSymbol.name) {
-							res.changes![`${documentURI}`].push({
-								range: parameter.selectionRange,
-								newText: params.newName
-							});
-						}
-					}
-				}
-				break;
-			}
+		for (const range of this.server.symbolManager.findVariableOrConstantUses(symbolsAt, oldSymbol)) {
+			res.changes![params.textDocument.uri].push({ range: range, newText: params.newName });
 		}
 
 		return res;
