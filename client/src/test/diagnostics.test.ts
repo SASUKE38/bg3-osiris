d0d436b37e4784ee24f044f039fa98e3388f8c49
SASUKE38 <bpstern@uci.edu>
2026-04-30 11:43:55 -0700

    test setup

---
+++ client/src/test/diagnostics.test.ts
@@ -0,0 +1,39 @@
+import * as vscode from "vscode";
+import * as assert from "assert";
+import { getDocUri, activate } from "./helper";
+
+suite("Should get diagnostics", () => {
+	const docUri = getDocUri("diagnostics.txt");
+
+	test("Diagnoses semicolon absence", async () => {
+		await testDiagnostics(docUri, [
+			{
+				message: "Unexpected token; expected ';'",
+				range: toRange(6, 0, 6, 30),
+				severity: vscode.DiagnosticSeverity.Error,
+				source: "osiris"
+			}
+		]);
+	});
+});
+
+function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
+	const start = new vscode.Position(sLine, sChar);
+	const end = new vscode.Position(eLine, eChar);
+	return new vscode.Range(start, end);
+}
+
+async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.Diagnostic[]) {
+	await activate(docUri);
+
+	const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+
+	assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+
+	expectedDiagnostics.forEach((expectedDiagnostic, i) => {
+		const actualDiagnostic = actualDiagnostics[i];
+		assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
+		assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
+		assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
+	});
+}
---
+++ client/src/test/index.ts
@@ -0,0 +1,32 @@
+import * as path from "path";
+import * as Mocha from "mocha";
+import { glob } from "glob";
+
+export function run(): Promise<void> {
+	const mocha = new Mocha({
+		ui: "tdd",
+		color: true
+	});
+	mocha.timeout(100000);
+
+	const testsRoot = __dirname;
+
+	return glob.glob("**.test.js", { cwd: testsRoot }).then(async (files) => {
+		files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
+
+		try {
+			await new Promise<void>((resolve, reject) => {
+				mocha.run((failures) => {
+					if (failures > 0) {
+						reject(`${failures} tests failed.`);
+					} else {
+						resolve();
+					}
+				});
+			});
+		} catch (err) {
+			console.error(err);
+			throw err;
+		}
+	});
+}
