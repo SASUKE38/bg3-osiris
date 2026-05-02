318490a42561825e354abebf289a7a500be3f6d1
SASUKE38 <bpstern@uci.edu>
2026-05-02 08:58:19 -0700

    references test setup

--- client/src/test/tests/referencesProvider.test.ts
+++ client/src/test/tests/referencesProvider.test.ts
@@ -5,6 +5,36 @@ import { getDocUri, activate, toRange } from "../helper";
 suite("References Provider", () => {
 	const docUri = getDocUri("Act3c_EPI_Emperor.txt");
 
+	suite("References", () => {
+		test("Finding references of constant should get all instances of the constant in the workspace", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+
+		test("Finding references of signature should get all instances of the signature in the workspace", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+
+		test("Finding references of variable should get all instances of the variable in the containing rule", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+
+		test("Finding references of number should get no references", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+
+		test("Finding references of string should get no references", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+
+		test("Finding references of enum should get no references", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+
+		test("Finding references of type should get no references", async () => {
+			await testReferences(docUri, new vscode.Position(0, 0), []);
+		});
+	});
+
 	suite("Document Highlight", () => {
 		test("Cursor on constant should highlight all instances of the constant in the document", async () => {
 			await testDocumentHighlight(docUri, new vscode.Position(24, 64), [
@@ -44,6 +74,22 @@ suite("References Provider", () => {
 		});
 	});
 });
+
+async function testReferences(
+	docUri: vscode.Uri,
+	position: vscode.Position,
+	expectedLocationList: vscode.Location[]
+) {
+	await activate(docUri);
+
+	const actualLocationList = (await vscode.commands.executeCommand(
+		"vscode.executeReferenceProvider",
+		docUri,
+		position
+	)) as vscode.Location[];
+
+	assert.deepStrictEqual(actualLocationList, expectedLocationList);
+}
 
 async function testDocumentHighlight(
 	docUri: vscode.Uri,
