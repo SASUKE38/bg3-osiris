a77a8aca5ca4395e2ce237439e0d66ea9ea9cd24
SASUKE38 <99054872+SASUKE38@users.noreply.github.com>
2026-05-20 22:19:36 -0700

    snippet tweaks, added diagnostic codes

---
+++ server/src/components/diagnostics/diagnosticCode.ts
@@ -0,0 +1,125 @@
+/**
+ * Collection of error codes. Based on:
+ * https://github.com/Norbyte/lslib/blob/6f5f6987d10d8485876b8368431c948e4dea4f83/LSLib/LS/Story/Compiler/CompilationContext.cs#L183
+ */
+export const enum DiagnosticCode {
+	// Miscellaenous internal error - should not happen.
+	InternalError = "E00",
+
+	// A type ID was declared multiple times in the story definition file.
+	TypeIdAlreadyDefined = "E01",
+
+	// A type name (alias)  was declared multiple times in the story definition file.
+	TypeNameAlreadyDefined = "E02",
+
+	// The type ID is either an intrinsic ID or is outside the allowed range.
+	TypeIdInvalid = "E03",
+
+	// The alias type ID doesn't point to a valid intrinsic type ID
+	IntrinsicTypeIdInvalid = "E04",
+
+	// A function with the same signature already exists.
+	SignatureAlreadyDefined = "E05",
+
+	// The type of an argument could not be resolved in a builtin function.
+	// (This only occurs when parsing story headers, not in goal code)
+	UnresolvedTypeInSignature = "E06",
+
+	// A goal with the same name was seen earlier.
+	GoalAlreadyDefined = "E07",
+
+	// The parent goal specified in the goal script was not found.
+	UnresolvedGoal = "E08",
+
+	// Failed to infer the type of a rule-local variable.
+	UnresolvedVariableType = "E09",
+
+	// The function signature (full typed parameter list) of a function
+	// could not be determined. This is likely the result of a failed type inference.
+	UnresolvedSignature = "E10",
+
+	// The intrinsic type of a function parameter does not match the expected type.
+	LocalTypeMismatch = "E11",
+
+	// Value with unknown type encountered during IR generation.
+	UnresolvedType = "E12",
+
+	// PROC/QRY declarations must start with a PROC/QRY name as the first condition.
+	InvalidProcDefinition = "E13",
+
+	// Fact contains a function that is not callable
+	// (the function is not a call, database or proc).
+	InvalidSymbolInFact = "E14",
+
+	// Rule action contains a function that is not callable
+	// (the function is not a call, database or proc).
+	InvalidSymbolInStatement = "E15",
+
+	// "NOT" action contains a non-database function.
+	CanOnlyDeleteFromDatabase = "E16",
+
+	// Initial PROC/QRY/IF function type differs from allowed type.
+	InvalidSymbolInInitialCondition = "E17",
+
+	// Condition contains a function that is not a query or database.
+	InvalidFunctionTypeInCondition = "E18",
+
+	// Function name could not be resolved.
+	UnresolvedSymbol = "E19",
+
+	// Use of less/greater operators on strings or guidstrings.
+	StringLtGtComparison = "W20",
+
+	// The alias type of a function parameter does not match the expected type.
+	GuidAliasMismatch = "E21",
+
+	// Object name GUID is prefixed with a type that is not known.
+	GuidPrefixNotKnown = "W22",
+
+	// PROC_/QRY_ naming style violation.
+	RuleNamingStyle = "W23",
+
+	// A rule variable was used in a read context, but was not yet bound.
+	ParamNotBound = "E24",
+
+	// The database is likely unused or unpopulated.
+	// (Written but not read, or vice versa)
+	UnusedDatabaseWarning = "W25",
+
+	// The database is likely unused or unpopulated.
+	// (Written but not read, or vice versa)
+	UnusedDatabaseError = "E25",
+
+	// Database "DB_" naming convention violation.
+	DbNamingStyle = "W26",
+
+	// Object name GUID could not be resolved to a game object.
+	UnresolvedGameObjectName = "W27",
+
+	// Type of name GUID differs from type of game object.
+	GameObjectTypeMismatch = "W28",
+
+	// Name part of name GUID differs from name of game object.
+	GameObjectNameMismatch = "W29",
+
+	// Multiple definitions seen for the same function with different signatures.
+	ProcTypeMismatch = "E30",
+
+	// Attempted to cast a type to an unrelated/incompatible type (i.e. STRING to INTEGER)
+	CastToUnrelatedType = "E31",
+
+	// Attempted to cast an alias to an unrelated alias (i.e. CHARACTERGUID to ITEMGUID)
+	CastToUnrelatedGuidAlias = "E32",
+
+	// Left-hand side and right-hand side variables are the same in a binary operation.
+	// This will result in an "invalid compare" error in runtime.
+	BinaryOperationSameRhsLhs = "E33",
+
+	// comparison on types that have known bugs or side effects
+	// (currently this only triggers on GUIDSTRING - STRING comparison)
+	RiskyComparison = "E34",
+
+	// The database is possibly used in an incorrect way.
+	// (Deleted and read, but not written)
+	UnwrittenDatabase = "W35"
+}
--- server/src/components/diagnostics/diagnosticsProvider.ts
+++ server/src/components/diagnostics/diagnosticsProvider.ts
@@ -3,21 +3,56 @@ import { ComponentBase } from "../../componentBase";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import { decodePath } from "../../utils/pathUtils";
 
+/*
+=== Header Only ===
+TypeIdAlreadyDefined 1
+TypeNameAlreadyDefined 2
+TypeIdInvalid 3
+IntrinsicTypeIdInvalid 4
+UnresolvedTypeInSignature 6
+
+=== Parser Updates ===
+GoalAlreadyDefined 7
+UnresolvedGoal 8
+InvalidProcDefinition 13
+InvalidSymbolInFact 14
+InvalidSymbolInStatement 15
+CanOnlyDeleteFromDatabase 16
+InvalidSymbolInInitialCondition 17
+InvalidFunctionTypeInCondition 18
+StringLtGtComparison 20
+RuleNamingStyle 23
+DbNamingStyle 26
+ProcTypeMismatch 30
+CastToUnrelatedType 31
+BinaryOperationSameRhsLhs 33
+RiskyComparison 34
+
+=== Analyzer-Specific ===
+UnresolvedVariableType 9
+UnresolvedSignature 10
+LocalTypeMismatch 11
+UnresolvedSymbol 19
+ParamNotBound 24
+UnusedDatabaseWarning 25
+CastToUnrelatedGuidAlias 32
+UnwrittenDatabase 35
+
+=== Unknown or Not Possible in Context ===
+SignatureAlreadyDefined 5
+UnresolvedType 12
+GuidAliasMismatch 21
+GuidPrefixNotKnown 22
+UnresolvedGameObjectName 27
+GameObjectTypeMismatch 28
+GameObjectNameMismatch 29
+*/
+
 export class DiagnosticProvider extends ComponentBase {
 	connection?: Connection;
 
 	getCapabilities(): Partial<ServerCapabilities> {
 		return {};
-	}
-
-	async validateTextDocument(textDocument: TextDocument) {
-		const resource = this.server.modManager.findResource(decodePath(textDocument.uri));
-		if (resource) {
-			await this.connection?.sendDiagnostics({
-				uri: textDocument.uri,
-				diagnostics: await resource.getDiagnostics()
-			});
-		}
 	}
 
 	initializeComponent(connection: Connection): void {
@@ -46,7 +81,13 @@ export class DiagnosticProvider extends ComponentBase {
 		}
 	};
 
-	handleDiagnostics(event: TextDocumentChangeEvent<TextDocument>) {
-		this.validateTextDocument(event.document);
+	async handleDiagnostics(event: TextDocumentChangeEvent<TextDocument>) {
+		const resource = this.server.modManager.findResource(decodePath(event.document.uri));
+		if (resource) {
+			await this.connection?.sendDiagnostics({
+				uri: event.document.uri,
+				diagnostics: await resource.getDiagnostics()
+			});
+		}
 	}
 }
