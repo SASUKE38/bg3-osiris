fbfcb2acb4aa8f4d0d36a74aeacdd2aae6a5e069
SASUKE38 <99054872+SASUKE38@users.noreply.github.com>
2026-06-08 23:06:48 -0700

    added ignored orphan query reading for dependencies

--- server/src/mods/dependency.ts
+++ server/src/mods/dependency.ts
@@ -1,10 +1,11 @@
-import { rmSync } from "fs";
+import { readFileSync, rmSync } from "fs";
 import { extractFromPak, extractPathsInPackage, extractStory } from "../utils/edge";
 import { Story } from "./story";
 
 export class Dependency {
 	private path;
 	private goalNames: string[] = [];
+	private ignoredOrphans: string[] = [];
 	private story?: Story;
 
 	constructor(path: string) {
@@ -13,13 +14,23 @@ export class Dependency {
 
 	async initialize() {
 		this.goalNames = await extractPathsInPackage(this.path, "RawFiles");
-		const osi = await extractFromPak(this.path, "story.div.osi");
-		if (osi === "") {
+		const osiPath = await extractFromPak(this.path, "story.div.osi");
+		if (osiPath === "") {
 			console.error(`Dependency ${this.path} missing story file`);
 			return;
 		}
 
-		this.story = await extractStory(osi);
-		rmSync(osi);
+		this.story = await extractStory(osiPath);
+		const ignoredOrphansPath = await extractFromPak(this.path, "story_orphanqueries_ignore_local.txt");
+		if (ignoredOrphansPath !== "") {
+			for (const line of readFileSync(ignoredOrphansPath, { encoding: "utf-8" }).split(/\r?\n/)) {
+				for (const database of line.split(/\s+[0-9]/)) {
+					if (database !== "") this.ignoredOrphans.push(database);
+				}
+			}
+			rmSync(ignoredOrphansPath);
+			console.log(this.ignoredOrphans);
+		}
+		rmSync(osiPath);
 	}
 }
