e383425b6f0248736e2079f59790d4c80855f8f0
SASUKE38 <99054872+SASUKE38@users.noreply.github.com>
2026-07-05 22:57:16 -0700

    dependency initialization for no osi mods

--- server/src/mods/dependency.ts
+++ server/src/mods/dependency.ts
@@ -10,6 +10,7 @@ export class Dependency {
 	readonly foundOrphans: string[] = [];
 	readonly activeGoals = new Map<number, string>();
 	readonly definedSignatures = new Map<string, Map<string, FunctionSignature[]>>();
+	goalParents?: Map<string, string>;
 	story?: Story;
 
 	constructor(data: DependencyMetaCollectionEntry) {
@@ -19,40 +20,19 @@ export class Dependency {
 
 	async initialize() {
 		try {
+			this.readOrphanFile("/Story/story_orphanqueries_ignore_local.txt", this.ignoredOrphans);
+			this.readOrphanFile("/Story/story_orphanqueries_found.txt", this.foundOrphans);
+
 			const goalNames = (await extractPathsInPackage(this.path, this.internalPath + "/Story/RawFiles/Goals")).map(
 				(value) => value.substring(0, value.length - 4)
 			);
 			const osiPath = (await extractFromPak(this.path, this.internalPath + "/Story/story.div.osi"))
 				.OutputPaths[0];
-			if (!osiPath) return;
-
-			this.story = await extractStory(osiPath);
-			rmSync(osiPath);
-
-			this.readOrphanFile("/Story/story_orphanqueries_ignore_local.txt", this.ignoredOrphans);
-			this.readOrphanFile("/Story/story_orphanqueries_found.txt", this.foundOrphans);
-
-			for (const node of Object.values(this.story.nodes)) {
-				if (!node.ReferencedBy) continue;
-				for (const reference of node.ReferencedBy) {
-					const goalName = goalNames.find(
-						(value) => value === this.story?.goals[reference.GoalRef.Index]?.Name
-					);
-					if (!goalName || this.definedSignatures.has(node.Name)) continue;
-
-					this.activeGoals.set(reference.GoalRef.Index, goalName);
-
-					const definitions = this.story.functions.filter((value) => value.Name.Name === node.Name);
-					if (!this.definedSignatures.has(goalName)) {
-						this.definedSignatures.set(goalName, new Map<string, FunctionSignature[]>());
-					}
-					const signatureMap = this.definedSignatures.get(goalName) as Map<string, FunctionSignature[]>;
-
-					signatureMap.set(
-						node.Name,
-						definitions.map((value) => value.Name)
-					);
-				}
+			if (osiPath) {
+				this.initializeWithStory(goalNames, osiPath);
+			} else {
+				this.goalParents = new Map<string, string>();
+				this.initializeWithoutStory(goalNames);
 			}
 		} catch (error) {
 			console.error(`An error occurred while initializing dependency ${this.path}: ${error}`);
@@ -69,5 +49,53 @@ export class Dependency {
 			}
 		}
 		rmSync(orphanPath);
+	}
+
+	private async initializeWithoutStory(goalNames: string[]) {
+		for (let i = 0; i < goalNames.length; i++) {
+			this.activeGoals.set(i, goalNames[i]);
+			const goalPath = await extractFromPak(
+				this.path,
+				`${this.internalPath}/Story/RawFiles/Goals/${goalNames[i]}.txt`
+			);
+
+			const matched = readFileSync(goalPath.OutputPaths[0], { encoding: "utf-8" }).match(
+				/ParentTargetEdge ("[a-zA-Z0-9_-]*")?/
+			);
+			if (matched) {
+				const parent = matched[0].split(" ")[1];
+				this.goalParents?.set(goalNames[i], parent.substring(1, parent.length - 1));
+			} else {
+				this.goalParents?.set(goalNames[i], "");
+			}
+
+			rmSync(goalPath.OutputPaths[0]);
+		}
+	}
+
+	private async initializeWithStory(goalNames: string[], osiPath: string) {
+		this.story = await extractStory(osiPath);
+		rmSync(osiPath);
+
+		for (const node of Object.values(this.story.nodes)) {
+			if (!node.ReferencedBy) continue;
+			for (const reference of node.ReferencedBy) {
+				const goalName = goalNames.find((value) => value === this.story?.goals[reference.GoalRef.Index]?.Name);
+				if (!goalName || this.definedSignatures.has(node.Name)) continue;
+
+				this.activeGoals.set(reference.GoalRef.Index, goalName);
+
+				const definitions = this.story.functions.filter((value) => value.Name.Name === node.Name);
+				if (!this.definedSignatures.has(goalName)) {
+					this.definedSignatures.set(goalName, new Map<string, FunctionSignature[]>());
+				}
+				const signatureMap = this.definedSignatures.get(goalName) as Map<string, FunctionSignature[]>;
+
+				signatureMap.set(
+					node.Name,
+					definitions.map((value) => value.Name)
+				);
+			}
+		}
 	}
 }
